80ffc1a417b6f784daf6dbd46ffea1d7e89da623
Michael Plunkett <michael@mrplunkett.com>
2020-08-13 12:40:07 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# React Template
+A template for building a react app, setup with Webpack and modules for handling .csv files, images, and fonts.
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class App extends React.Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <>
+      <h1>Hi there</h1>
+      </>
+    )
+  }
+}
+
+export default App
