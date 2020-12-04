3ff69b864b3e242af7b2b8574557312aeec344c0
mister-blanket <michael@mrplunkett.com>
2020-12-03 19:49:33 -0600

    Consistent shadow for mobile

--- movingShadow.js
+++ movingShadow.js
@@ -67,11 +67,20 @@
   };
 
   const getElePos = element => {
-    var rect = element.getBoundingClientRect();
-    rect.centerX = Math.round(rect.x + (rect.width/2));
-    rect.centerY = Math.round(rect.y + (rect.height/2));
+    if (event.type === 'deviceorientation') {
+      // Get device window specs
+      const rect = {
+        centerX: Math.round(window.innerWidth/2),
+        centerY: Math.round(window.innerHeight/2)
+      };
+      return rect;
+    } else {
+      var rect = element.getBoundingClientRect();
+      rect.centerX = Math.round(rect.x + (rect.width/2));
+      rect.centerY = Math.round(rect.y + (rect.height/2));
 
-    return rect;
+      return rect;
+    }
   };
 
   const calculateDistance = (element, viewPos, { angle=20 } = {}) => {
--- src/getElePos.js
+++ src/getElePos.js
@@ -1,9 +1,22 @@
 const getElePos = element => {
-  var rect = element.getBoundingClientRect();
-  rect.centerX = Math.round(rect.x + (rect.width/2));
-  rect.centerY = Math.round(rect.y + (rect.height/2));
 
-  return rect;
+  // Use same shadow for mobile
+  if (event.type === 'deviceorientation') {
+    // Get device window specs
+    const rect = {
+      centerX: Math.round(window.innerWidth/2),
+      centerY: Math.round(window.innerHeight/2)
+    }
+    return rect;
+
+  // Create different shadows for desktop
+  } else {
+    var rect = element.getBoundingClientRect();
+    rect.centerX = Math.round(rect.x + (rect.width/2));
+    rect.centerY = Math.round(rect.y + (rect.height/2));
+
+    return rect;
+  }
 }
 
 export default getElePos;
